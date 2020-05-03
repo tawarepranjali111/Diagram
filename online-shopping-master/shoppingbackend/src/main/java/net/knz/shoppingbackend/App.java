9d0681e1d8899e82a35d0a26b39b904dfd04675c
tawarepranjali111 <61134743+tawarepranjali111@users.noreply.github.com>
2020-05-03 14:54:06 +0530

    Add files via upload
    Front end of application completed

---
+++ online-shopping-master/shoppingbackend/src/main/java/net/knz/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.knz.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
